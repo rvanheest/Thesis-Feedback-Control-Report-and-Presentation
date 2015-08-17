8b3705614cd0046e9bdef1e6f1d5cc3076158730
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-08-17 15:14:21 +0200

    RunningAverage as an example of the Component interface
    Based on different implementations

---
+++ workspace/src/main/scala/presentation/RunningAverage.scala
@@ -0,0 +1,87 @@
+package presentation
+
+import applied_duality.reactive.Observable
+import scala.collection.mutable.Queue
+import applied_duality.reactive.Subject
+import applied_duality.reactive.Observer
+
+/*
+ * Immutable Component interface
+ */
+trait ImmutableComponent[I, O] {
+	def update(i: I): ImmutableComponent[I, O]
+	def action: O
+}
+class ImmutableRunningAverage(n: Int, queue: Queue[Double]) extends ImmutableComponent[Double, Double] {
+
+	def update(u: Double): ImmutableRunningAverage = {
+		if (queue.length == n) queue.dequeue
+		queue.enqueue(u)
+
+		new ImmutableRunningAverage(n, queue)
+	}
+
+	def action: Double = queue.sum / queue.length
+}
+
+/*
+ * Mutable Component interface
+ */
+trait MutableComponent[I, O] {
+	def update(i: I): Unit
+	def action: O
+}
+class MutableRunningAverage(n: Int) extends MutableComponent[Double, Double] {
+
+	val queue = Queue[Double]()
+
+	def update(u: Double): Unit = {
+		if (queue.size == n) queue.dequeue
+		queue.enqueue(u)
+	}
+
+	def action: Double = queue.sum / queue.length
+}
+
+/*
+ * Reactive Component interface
+ */
+trait ReactiveComponent[I, O] {
+	def in(i: I): Unit
+	def out: Observable[O]
+}
+class ReactiveRunningAverage(n: Int) extends ReactiveComponent[Double, Double] {
+
+	val queue = Queue[Double]()
+	val output = Subject[Double]
+
+	def in(u: Double): Unit = {
+		if (queue.size == n) queue.dequeue()
+		queue.enqueue(u)
+
+		output.onNext(queue.sum / queue.length)
+	}
+	def out: Observable[Double] = output
+}
+
+/*
+ * The real Component interface
+ */
+trait Component[I, O] extends Observer[I] {
+	val subject = Subject[I]
+	val observable = subject.publish(transform)
+
+	def transform(i: Observable[I]): Observable[O]
+	def asObservable: Observable[O] = observable
+
+	override def onNext(i: I) = subject.onNext(i)
+	override def onError(e: Throwable) = subject.onError(e)
+	override def onCompleted = subject.onCompleted
+}
+class RunningAverage(n: Int) extends Component[Double, Double] {
+	val queue = Queue[Double]()
+
+	def transform(ts: Observable[Double]) = ts.tee(_ => if (queue.length == n) queue.dequeue)
+		.tee(queue.enqueue(_))
+		.map(t => queue.sum / queue.size)
+}
