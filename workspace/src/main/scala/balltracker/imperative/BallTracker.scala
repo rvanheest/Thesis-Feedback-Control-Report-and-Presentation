877fd2370e7febb42e5067e7fc36379e45721e7a
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-10-21 15:14:00 +0200

    comments and cleanup

--- workspace/src/main/scala/balltracker/imperative/BallTracker.scala
+++ workspace/src/main/scala/balltracker/imperative/BallTracker.scala
@@ -25,41 +25,42 @@ class BallTracker extends Application {
 		var y = 20.0
 		var vx = 0.0
 		var vy = 0.0
-	
+
 		var setpointX = x
 		var setpointY = y
 		var prevErrorX = 0.0
 		var prevErrorY = 0.0
 		var integralX = 0.0
 		var integralY = 0.0
-	
+
 		var kp = 3.0
 		var ki = 0.0001
 		var kd = 80.0
-	
+
 		val history = new History
 		var historyTick = 0
-	
+
 		def pid: Acceleration = {
 			val errorX = setpointX - x
 			integralX += errorX
 			val derivativeX = errorX - prevErrorX
 			prevErrorX = errorX
-	
+
 			val errorY = setpointY - y
 			integralY += errorY
 			val derivativeY = errorY - prevErrorY
 			prevErrorY = errorY
-			
+
 			def pid(error: Double, integral: Double, derivative: Double) = {
 				0.001 * (kp * error + ki * integral + kd * derivative)
 			}
 
 			(pid(errorX, integralX, derivativeX), pid(errorY, integralY, derivativeY))
 		}
-	
+
 		@tailrec
 		def update(implicit gc: GraphicsContext): Unit = {
+			// going from acceleration to position by a double integral
 			var a = pid
 			var ax = a._1
 			var ay = a._2
@@ -70,16 +71,18 @@ class BallTracker extends Application {
 			vy += ay
 			x += vx
 			y += vy
-	
+
+			// managing the history
 			historyTick += 1
 			if (historyTick == 5) {
 				historyTick = 0
-				
+
 				if (history.size >= 50)
 					history.dequeue
 				history.enqueue((x, y))
 			}
 
+			// drawing all the elements
 			Platform.runLater(() => {
 				Draw.drawBackground
 				Draw.drawHistory(history)
@@ -87,14 +90,10 @@ class BallTracker extends Application {
 				Draw.drawBall((x, y))
 				Draw.drawVectors((x, y), (ax, ay))
 			})
-			
+
+			// sleep and call again
 			Thread.sleep(16)
 			update
-		}
-
-		def canvasClick(e: MouseEvent): Unit = {
-			setpointX = e.getX
-			setpointY = e.getY
 		}
 
 		val canvas = new Canvas(width, height)
@@ -102,11 +101,15 @@ class BallTracker extends Application {
 
 		val root = new StackPane(canvas)
 		root.setAlignment(Pos.TOP_LEFT)
-		root.addEventHandler(MouseEvent.MOUSE_CLICKED, (e: MouseEvent) => canvasClick(e))
+		root.addEventHandler(MouseEvent.MOUSE_CLICKED, (e: MouseEvent) => {
+			// change setpoint on click
+			setpointX = e.getX
+			setpointY = e.getY
+		})
 
 		val loop = new Thread(() => update)
 
-		stage.setOnHidden((_ : WindowEvent) => System.exit(0))
+		stage.setOnHidden((_: WindowEvent) => System.exit(0))
 		stage.setScene(new Scene(root, width, height))
 		stage.setTitle("Balltracker")
 		stage.show()
