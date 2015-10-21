10de7eeb55e3c1c41f3fe3db6efe8411bdf9dbf0
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-10-21 12:12:00 +0200

    Util classes

---
+++ workspace/src/main/scala/javafx/rx/rx.scala
@@ -0,0 +1,28 @@
+package javafx
+
+import applied_duality.reactive.Observable
+import applied_duality.reactive.Subscription
+import javafx.event.Event
+import javafx.event.EventHandler
+import javafx.event.EventType
+import javafx.scene.Node
+import javafx.scene.input.InputEvent
+import javafx.scene.input.MouseEvent
+
+package object rx {
+
+	implicit def toHandler[T <: Event](action: T => Unit): EventHandler[T] = {
+		new EventHandler[T] { override def handle(e: T): Unit = action(e) }
+	}
+
+	implicit class Events(val node: Node) extends AnyVal {
+
+		def getEvent[T <: InputEvent](event: EventType[T]): Observable[T] = Observable.create[T](observer => {
+			val handler = (e: T) => observer.onNext(e)
+			node.addEventHandler(event, handler)
+			observer += Subscription { node.removeEventHandler(event, handler) }
+		})
+
+		def mouseClicked: Observable[MouseEvent] = getEvent(MouseEvent.MOUSE_CLICKED)
+	}
+}
