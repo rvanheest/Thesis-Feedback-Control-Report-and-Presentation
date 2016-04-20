80fb3d2e18fb3ebd1386ed6d600efd37ae31d9d5
Richard van Heest <richard.van.heest@dans.knaw.nl>
2016-04-20 18:52:05 +0200

    update as an inner-function for tail recursion purposes

--- workspace/src/main/scala/report/balltracker/imperative/BallTracker.scala
+++ workspace/src/main/scala/report/balltracker/imperative/BallTracker.scala
@@ -9,6 +9,8 @@ import javafx.scene.input.MouseEvent
 import javafx.scene.layout.StackPane
 import javafx.stage.{Stage, WindowEvent}
 
+import scala.annotation.tailrec
+
 class BallTracker extends Application {
 
   var ball = Ball(ballRadius)
@@ -34,31 +36,32 @@ class BallTracker extends Application {
     (error * kp + integral * ki + derivative * kd) * 0.001
   }
 
-  def update(implicit gc: GraphicsContext): Unit = {
-    val acceleration = pid.map(a => math.max(math.min(a, 0.2), -0.2))
-    ball = ball accelerate acceleration
-
-    // managing the history
-    historyTick += 1
-    if (historyTick == 5) {
-      historyTick = 0
-
-      if (history.size >= 50)
-        history.dequeue
-      history enqueue ball.position
-    }
-
-    // drawing all the elements
-    Platform.runLater(() => Draw.draw(ball.position, setpoint, ball.acceleration, history))
-
-    // sleep and call again
-    Thread.sleep(16)
-    update
-  }
-
   def start(stage: Stage) = {
     val canvas = new Canvas(width, height)
     implicit val gc = canvas.getGraphicsContext2D
+
+    @tailrec
+    def update(implicit gc: GraphicsContext): Unit = {
+      val acceleration = pid.map(a => math.max(math.min(a, 0.2), -0.2))
+      ball = ball accelerate acceleration
+
+      // managing the history
+      historyTick += 1
+      if (historyTick == 5) {
+        historyTick = 0
+
+        if (history.size >= 50)
+          history.dequeue
+        history enqueue ball.position
+      }
+
+      // drawing all the elements
+      Platform.runLater(() => Draw.draw(ball.position, setpoint, ball.acceleration, history))
+
+      // sleep and call again
+      Thread.sleep(16)
+      update
+    }
 
     val root = new StackPane(canvas)
     root.setAlignment(Pos.TOP_LEFT)
